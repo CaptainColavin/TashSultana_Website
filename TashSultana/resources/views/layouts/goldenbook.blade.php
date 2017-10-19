eec6169c08fc6eb339b7d74f65cde137c71fa8c8
QYing <ying.qixin@gmail.com>
2017-10-19 11:49:23 +0200

    guestbook front

--- TashSultana/resources/views/layouts/goldenbook.blade.php
+++ TashSultana/resources/views/layouts/goldenbook.blade.php
@@ -1,43 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Golden Book</h1>
-<div class="table-responsive">
-  <table class="table table-striped table-data">
-    <thead style="background-color: #DCDCDC; color: black;">
-      <tr>
-        <th>Comment</th>
-        <th>Date</th>
-        <th>User</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach($goldenbook as $comment)
-    <tr>
-      <td>
-        {{ $comment->text }}
-      </td>
-      <td>
-        {{ $comment->date }}
-      </td>
-      <td>
-        {{ $comment->user->name }}
-      </td>
-    </tr>
-    @endforeach
-    </tbody>
-  </table>
-
-  @if (Auth::check())
-      <form action="/goldenbook" method="POST">
-      {{csrf_field()}}
-      <label for="text">Comment :</label>
-      <textarea class ="form-control" id="textArea" name="text" rows="5"></textarea>
-    <br>
-    <button type="submit" class="btn">Submit</button>
-    </form>
-  @else
-    <p>Please login to post a comment.</p>
-  @endif
+<h1>Guestbook</h1>
+<div class="comment-container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th class="title-user">User</th>
+                <th class="title-comment">Comment</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($goldenbook as $comment)
+            <tr>
+                <td class="user-infos">
+                    <div class="user-name">{{ $comment->user->name }}</div>
+                    <div class="user-date">{{ $comment->date }}</div>
+                </td>
+                <td class="user-comment">
+                    {{ $comment->text }}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
+@if (Auth::check())
+<form action="/guestbook" method="POST">
+    {{csrf_field()}}
+    <label for="text">Comment :</label>
+    <textarea class="form-control comment-text-area" id="textArea" name="text" rows="5"></textarea>
+
+    <button type="submit" class="btn">Submit</button>
+</form>
+@else
+<p>Please login to post a comment.</p>
+@endif
+
 @stop
